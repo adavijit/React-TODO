e46afb4e755b03a01dc5b90207ae55be406952a2
Avijit Das <adavijit@yahoo.com>
2019-08-04 23:25:03 +0530

    fix3

--- src/List.js
+++ src/List.js
@@ -5,13 +5,12 @@ export default class List extends Component {
     render() {
         return(
 
-            <div>
                 <ul id="myUL">
                     {this.props.todos.map((todo, index) => (
                         <li key={index}>{todo}</li>
                     ))}
                 </ul>
-            </div>
+           
         )
     }
 }
